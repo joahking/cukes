21869eae157dad662ae2025d99dc7c4fb0d469c6
Joaquin Rivera Padron <joahking@gmail.com>
2009-02-22 15:36:18 +0100

    cukes app is ready

--- app/controllers/cukes_controller.rb
+++ app/controllers/cukes_controller.rb
@@ -1,2 +1,8 @@
 class CukesController < ApplicationController
+  def index
+    respond_to do |format|
+      format.html # index.html.erb
+      format.js { render :text => "ajaxed cukes" }
+    end
+  end
 end
